5b98044b20959b6b850d8c4a16c38acbe9b0031f
riyan <khanshafique.ahamed@gmail.com>
2021-12-06 12:01:08 +0530

    added more functions to cron

--- userManagement/cron.py
+++ userManagement/cron.py
@@ -6,6 +6,7 @@ from datetime import date, datetime
 
 def delete_unverified_user():
     users = UnVerifiedUser.objects.all()
+    print("|cronjob| delete unvierified users fired")
     for user in users:
         if user.Generated_Date < datetime.now():
             user.delete()
