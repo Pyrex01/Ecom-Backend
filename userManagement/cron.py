6ce41ef202d8e2af9731f82a8b72f2fc67e0d906
riyan <khanshafique.ahamed@gmail.com>
2021-12-07 21:43:34 +0530

    blay

--- userManagement/cron.py
+++ userManagement/cron.py
@@ -9,7 +9,7 @@ def delete_unverified_user():
     users = UnVerifiedUser.objects.all()
     print("|cronjob| delete unvierified users fired")
     for user in users:
-        expression = user.Generated_Date.utcnow() > (datetime.now().utcnow()-timedelta(minutes=3)).utcnow()
+        expression = user.Generated_Date.minute > (datetime.now().minute-3)
         print(expression)
         if expression :
             user.delete()
