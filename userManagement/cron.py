2666a07a5367b9376f4a26f59323ca1ac7d08492
riyan <khanshafique.ahamed@gmail.com>
2021-12-07 19:50:36 +0530

    added cron job

--- userManagement/cron.py
+++ userManagement/cron.py
@@ -1,12 +1,17 @@
 from django.contrib.auth.models import User
 from userManagement.models import *
-from datetime import date, datetime
+from datetime import time, timezone, datetime
+import pytz
 
-
+UTC = pytz.UTC
 
 def delete_unverified_user():
     users = UnVerifiedUser.objects.all()
     print("|cronjob| delete unvierified users fired")
     for user in users:
-        if user.Generated_Date < datetime.now():
+        print("user date",type(user.Generated_Date.utcnow()) , "systyem",type(datetime.now().utcnow()))
+        print("user data", user.Generated_Date.utcnow())
+        print("now date", datetime.now().utcnow())
+        
+        if user.Generated_Date.utcnow() < datetime.now().utcnow():
             user.delete()
