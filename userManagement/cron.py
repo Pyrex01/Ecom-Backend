5b38d2b4c83cd08c17cda5795b138590a62ae58e
riyan <khanshafique.ahamed@gmail.com>
2021-12-07 20:14:55 +0530

    more functions to cronjob

--- userManagement/cron.py
+++ userManagement/cron.py
@@ -9,9 +9,7 @@ def delete_unverified_user():
     users = UnVerifiedUser.objects.all()
     print("|cronjob| delete unvierified users fired")
     for user in users:
-        print("user date",type(user.Generated_Date.utcnow()) , "systyem",type(datetime.now().utcnow()))
-        print("user data", user.Generated_Date.utcnow())
-        print("now date", datetime.now().utcnow())
-        
-        if (user.Generated_Date.utcnow() + timedelta(minutes=3)) < datetime.now().utcnow() :
+        expression = (user.Generated_Date.utcnow() + timedelta(minutes=3)) < datetime.now().utcnow()
+        print(expression)
+        if expression :
             user.delete()
