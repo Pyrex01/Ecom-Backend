1cc24a7109ba18cae8114f053c5f296fae5f8374
riyan <khanshafique.ahamed@gmail.com>
2022-01-12 11:25:39 +0530

    added further url

--- store/pagination.py
+++ store/pagination.py
@@ -9,13 +9,14 @@ class ListPage(PageNumberPagination):
     max_page_size = 10000
 
     def get_next_link(self):
+        url = self.request.build_absolute_uri('https://shopingbazar.sytes.net/store/getItems/') 
         if not self.page.has_next():
             return None
-        url = self.request.build_absolute_uri('https://shopingbazar.sytes.net')
         page_number = self.page.next_page_number()
         return replace_query_param(url, self.page_query_param, page_number)
     
     def get_previous_link(self):
+        url = self.request.build_absolute_uri('https://shopingbazar.sytes.net/store/getItems/')
         if not self.page.has_previous():
             return None
         page_number = self.page.previous_page_number()
