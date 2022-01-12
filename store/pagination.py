fb9e4a61a98f86f940839d588c120ff9992cf694
riyan <khanshafique.ahamed@gmail.com>
2022-01-12 10:38:09 +0530

    addded more data to paginator

--- store/pagination.py
+++ store/pagination.py
@@ -1,4 +1,5 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination ,OrderedDict
+from rest_framework.response import Response
 
 
 
@@ -6,3 +7,11 @@ class ListPage(PageNumberPagination):
     page_size = 9
     page_size_query_param = 'page_size'
     max_page_size = 10000
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+             ('countItemsOnPage', self.page_size),
+             ('current', self.page.number),
+             ('next', self.get_next_link()),
+             ('previous', self.get_previous_link()),
+             ('results', data)
+         ]))
