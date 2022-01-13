1e1531899eb6ed11a8000ce1585d5cd95b0166e3
riyan <khanshafique.ahamed@gmail.com>
2022-01-13 10:58:59 +0530

    bug fix in sorted class

--- store/pagination.py
+++ store/pagination.py
@@ -23,6 +23,26 @@ class ListPage(PageNumberPagination):
         if page_number == 1:
             return remove_query_param(url, self.page_query_param)
         return replace_query_param(url, self.page_query_param, page_number)
+class ListPageSort(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
+
+    def get_next_link(self):
+        url = self.request.build_absolute_uri('http://shopingbazar.sytes.net/store/getSortItems/') 
+        if not self.page.has_next():
+            return None
+        page_number = self.page.next_page_number()
+        return replace_query_param(url, self.page_query_param, page_number)
+    
+    def get_previous_link(self):
+        url = self.request.build_absolute_uri('http://shopingbazar.sytes.net/store/getSortItems/')
+        if not self.page.has_previous():
+            return None
+        page_number = self.page.previous_page_number()
+        if page_number == 1:
+            return remove_query_param(url, self.page_query_param)
+        return replace_query_param(url, self.page_query_param, page_number)
 
     def get_paginated_response(self, data):
         return Response(OrderedDict([
