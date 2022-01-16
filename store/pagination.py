7f0cd2da734d1c849a420975b920a57cf3317cc4
riyan <khanshafique.ahamed@gmail.com>
2022-01-16 10:32:37 +0530

    pagination bug fixed but with categorie only

--- store/pagination.py
+++ store/pagination.py
@@ -2,7 +2,6 @@ from rest_framework.pagination import PageNumberPagination ,OrderedDict,replace_
 from rest_framework.response import Response
 
 
-
 class ListPage(PageNumberPagination):
     page_size = 2
     page_size_query_param = 'page_size'
@@ -39,14 +38,20 @@ class ListPageSort(PageNumberPagination):
     max_page_size = 10000
 
     def get_next_link(self):
-        url = self.request.build_absolute_uri('http://shopingbazar.sytes.net/store/getSortItems/') 
+        data=self.request.GET
+        url = self.request.build_absolute_uri('http://shopingbazar.sytes.net/store/getSortItems/?') 
+        if("categories" in data.keys()):
+            url += "categories="+data["categories"]
         if not self.page.has_next():
             return None
         page_number = self.page.next_page_number()
         return replace_query_param(url, self.page_query_param, page_number)
     
     def get_previous_link(self):
-        url = self.request.build_absolute_uri('http://shopingbazar.sytes.net/store/getSortItems/')
+        data=self.request.GET
+        url = self.request.build_absolute_uri('http://shopingbazar.sytes.net/store/getSortItems/?') 
+        if("categories" in data.keys()):
+            url += "categories="+data["categories"]
         if not self.page.has_previous():
             return None
         page_number = self.page.previous_page_number()
