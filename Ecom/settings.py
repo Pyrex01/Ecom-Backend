263ce390adfe3019ef8e67b3cb4b1ddf5ed4a2c9
riyan <khanshafique.ahamed@gmail.com>
2022-03-17 22:09:00 +0530

    admin panel color changed

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'drf_yasg',
+    'django_extensions',
     'django_crontab',
     'corsheaders',
     'userManagement.apps.UsermanagementConfig',
