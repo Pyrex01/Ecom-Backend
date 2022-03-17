596cbb8494b3c9478175fc92afdf5ce876831b71
riyan <khanshafique.ahamed@gmail.com>
2022-03-17 22:12:11 +0530

    removed git extention from settings

--- Ecom/settings.py
+++ Ecom/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'drf_yasg',
-    'django_extensions',
     'django_crontab',
     'corsheaders',
     'userManagement.apps.UsermanagementConfig',
